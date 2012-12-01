c8f63ec1199121ce6ccfa5bf850b877805b077ff
Will <will@will-VirtualBox.(none)>
2012-12-01 13:13:32 -0800

    uploading files to repo

---
+++ ma2.c
@@ -0,0 +1,28 @@
+﻿#include "cma.h"
+#include "stdlib.h"
+
+
+ 
+int main(int argc, char * argv[]) {
+          int i;
+          int n = 0; //to hold last good value for i
+	printf("Enter a number to specify size of the initial block of memory:\n");
+	scanf("%d", &i);
+          int size = atoi(argv[i]);
+          
+          class_memory(malloc(size),size); // give it to memory allocator
+                
+          while (i != '\0')
+          {
+                printf("i=%d\n",i);  //used to test while loop
+                n = -(i/2);
+                
+                i *= 2;
+                
+          }
+          
+ 
+printf("here is the value of the largest block allocated %d\n",n);
+          
+          
+}
---
+++ makefile
@@ -0,0 +1,44 @@
+CC=gcc
+TARGETS=ma ma2 ma.shared ma2.shared
+CFLAGS=-O2
+LDFLAGS=-L. -lcma
+STATIC=libcma.a
+SHARED=libcma.so
+DIRNAME=$$(pwd)
+BASE=`basename $(DIRNAME)`
+.PHONY: clean
+
+all: $(TARGETS)
+
+.c.o:
+	@echo Building $?
+	$(CC) $(CFLAGS) -c $?
+cma.o: cma.c
+	$(CC) -c cma.c
+
+$(STATIC): cma.o
+	$(AR) r $@ $?
+
+$(SHARED): cma.o
+	$(CC) -shared -o $@ $?
+
+ma.shared: ma.o $(SHARED)
+	$(CC) -o $@ ma.o $(LDFLAGS)
+
+ma2.shared: ma2.o $(SHARED)
+	$(CC) -o $@ ma2.o $(LDFLAGS)
+
+ma: ma.o $(STATIC)
+	$(CC) -static -o ma ma.o $(STATIC)
+
+ma2: ma2.o $(STATIC)
+	$(CC) -static -o ma2 ma2.o $(STATIC)
+
+dist: clean
+	tar -cvf cma_new.tar ../$(BASE)
+
+clean:
+	-rm *.o
+	-rm $(TARGETS)
+	-rm $(STATIC) $(SHARED)
+
